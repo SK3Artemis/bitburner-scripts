32e588ab4656ed4dd687865ad436a6dc3ccfa0a6
SK_Artemis <98838698+SK3Artemis@users.noreply.github.com>
2025-01-21 14:29:22 +0100

    Update git-pull.js

--- git-pull.js
+++ git-pull.js
@@ -1,6 +1,6 @@
 let options;
 const argsSchema = [
-    ['github', 'alainbryden'],
+    ['github', 'SK3Artemis'],
     ['repository', 'bitburner-scripts'],
     ['branch', 'main'],
     ['download', []], // By default, all supported files in the repository will be downloaded. Override with just a subset of files here
@@ -40,8 +40,10 @@ export async function main(ns) {
         else
             ns.tprint(`WARNING: "${fullLocalFilePath}" was not updated. (Currently running, or not located at ${remoteFilePath}?)`)
     }
-    ns.tprint(`INFO: Pull complete. If you have any questions or issues, create an issue on github or join the ` +
-        `Bitburner Discord channel "#Insight's-scripts": https://discord.com/channels/415207508303544321/935667531111342200`);
+    ns.tprint(`[Edit by SK_] ` +
+        `INFO: Pull complete. If you have any questions or issues, create an issue on github or join the ` +
+        `Bitburner Discord channel "#Insight's-scripts": https://discord.com/channels/415207508303544321/935667531111342200` +
+        `To start, type: "autopilot.js" `);
     // Remove any temp files / scripts from the prior version
     ns.run(pathJoin(options.subfolder, `cleanup.js`));
 }
